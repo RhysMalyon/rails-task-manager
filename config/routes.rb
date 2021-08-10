466f1772af2b43cb91c9d25c6dc9e573a1a79e55
Rhys Malyon <malyonr@gmail.com>
2021-08-10 14:22:49 +0900

    Add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
+  # get 'tasks', to: 'tasks#index', as: :tasks
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
